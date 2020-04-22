3c2d085a06dbb4571b282e1600e7888a8203811e
Jose Luis Rosas Peimbert <jlrosasp@gmail.com>
2020-04-21 20:55:53 -0500

    Carga Inicial del Proyecto Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
+import { Top5Component } from './pages/top5/top5.component';
+import { HistogramaComponent } from './pages/histograma/histograma.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'acerca-de', component: AcercaDeComponent },
+  { path: 'top5', component: Top5Component },
+  { path: 'histograma', component: HistogramaComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
+import { Top5Component } from './pages/top5/top5.component';
+import { HistogramaComponent } from './pages/histograma/histograma.component';
+import { NavbarComponent } from './components/navigation/navbar/navbar.component';
+import { LineChartComponent } from './components/charts/line-chart/line-chart.component';
+import { BarChartComponent } from './components/charts/bar-chart/bar-chart.component';
+import { PieChartComponent } from './components/charts/pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AcercaDeComponent,
+    Top5Component,
+    HistogramaComponent,
+    NavbarComponent,
+    LineChartComponent,
+    BarChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
+    ChartsModule,
     AppRoutingModule
   ],
   providers: [],
